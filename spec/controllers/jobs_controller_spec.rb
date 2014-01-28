b129922d71c7a24444f6e61df9121caea5fef417
Ilya Katz <ilyakatz@gmail.com>
2014-01-28 13:38:13 -0500

    test invalid request

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -9,4 +9,12 @@ describe JobsController do
     body_json["status"].should eq("submitted")
     Jobs.last.title.should eq("Developer")
   end
+
+  it "should throw an execption if job could not be created" do
+    post :create, format: :json, job_title: "Wrong"
+    assert_response 200
+    body_json = JSON.parse(response.body)
+    body_json["status"].should eq("failure")
+    body_json["message"].should eq("Invalid parameters")
+  end
 end

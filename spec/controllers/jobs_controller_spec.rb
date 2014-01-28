d19b5754e366bbb04d2f7d85b4a56136392b6974
Ilya Katz <ilyakatz@gmail.com>
2014-01-28 13:33:01 -0500

    full test for submitted job

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -4,6 +4,9 @@ describe JobsController do
 
   it "should create a job" do
     post :create, format: :json, title: "Developer"
+    assert_response 200
+    body_json = JSON.parse(response.body)
+    body_json["status"].should eq("submitted")
     Jobs.last.title.should eq("Developer")
   end
 end

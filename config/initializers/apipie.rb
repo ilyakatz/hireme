1e42475ccbae9c51f016fbb911e720572c0a88e8
Ilya Katz <ilyakatz@gmail.com>
2014-02-07 14:08:18 -0500

    some meta data for the documentation

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,9 @@
 Apipie.configure do |config|
   config.app_name                = "Hireme"
   config.api_base_url            = "/api"
-  config.doc_base_url            = "/apipie"
+  config.doc_base_url            = "/doc/api"
   # were is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/*.rb"
+  config.copyright               = "&copy; #{Time.now.year} <a href='http://ilyakatz.github.io/hireme '>Ilya Katz</a>"
+  config.app_info                = "This developer thinks that hiring needs an upgrade"
 end
